881ca5686c9e9332fb8ce0d8eacf318e7118b1c7
HeatherMikan <heather.mikan@gmail.com>
2021-03-21 11:49:12 -0400

    Added frontend navigation bar so it is ready for backend queries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,28 @@ function App() {
         CS4440 Project: Traffic Collision Data Visualizer
       </header>
       <Button>Hello</Button>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      </head>
+      <body>
+
+      <div class="sidenav">
+        <a href="#">Home</a>
+        <a href="#">Query One</a>
+        <a href="#">Query Two</a>
+        <a href="#">Query Three</a>
+        <a href="#">Query Four</a>
+        <a href="#">...</a>
+      </div>
+
+      <div class="main">
+        <h2>Sidenav Example</h2>
+        <p>This sidenav is always shown.</p>
+        <p>Tutorial from: https://www.w3schools.com/howto/howto_js_sidenav.asp .</p>
+        <p>We can easily adjust the width of this navbar in app.css.</p>
+      </div>
+
+      </body>
     </div>
   );
 }
