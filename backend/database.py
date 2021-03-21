8da45dccbbe1109f117e2a4570442f402440d941
Adam Chau <adamchau@bellsouth.net>
2021-03-21 17:22:07 -0400

    added a compressed file that contains the dataset used

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from pymongo import MongoClient
 import json
+import os
 
 def importdata():
     client = MongoClient("mongodb://localhost:27017")
     db = client["lacollisions"]
     collisionsdata = db["collisionsdata"]
-    df = pd.read_csv("traffic-collision-data-from-2010-to-present.csv")
-    print("Yes")
+    df = pd.read_csv("collisiondata/traffic-collision-data-from-2010-to-present.csv")
     jsondata = json.loads(df.to_json(orient='records'))
     collisionsdata.insert_many(jsondata)
 
