e4b5baea256a99212bd2907c80c3c217e56d7e15
Nabil Patel <npatel412@gatech.edu>
2021-04-14 11:17:55 -0400

    Attempt at Heatmap

--- frontend/src/components/HeatMap.js
+++ frontend/src/components/HeatMap.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useHistory, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class HeatMap extends React.Component {
+const { tableau } = window;
 
-    render() {
-        return(
-            <div>
-                HeatMap
-            </div>
-        )
+function HeatMap() {  
+
+    const url = "https://public.tableau.com/profile/nabil.patel#!/vizhome/LATrafficHeatMapTest/Sheet1";
+    const ref = useRef(null);
+    
+
+    const options = {
+        device: "desktop",
+    };
+
+    function initViz() {
+        new tableau.Viz(ref.current, url, options);
     }
-}
 
+    useEffect(() => {
+        initViz();
+    },[])
+
+    return (
+        <div>
+            <div ref={ref}></div>
+        </div>
+    );
+}
+// class HeatMap extends React.Component {
+//     render () {
+//         return (
+//             HeatMapMake()
+//         )
+//     }
+// }
 export default HeatMap;
