f7b8ec62c162f8214d1f8035f5515420ce961c4d
HeatherMikan <heather.mikan@gmail.com>
2021-04-16 15:34:15 -0400

    added hover to bar graph

--- frontend/src/components/BarGraph.js
+++ frontend/src/components/BarGraph.js
@@ -9,9 +9,16 @@ import {
 } from "react-vis";
 
 class BarGraph extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      index: null,
+    }
+  }
     render () {
         const data = this.props.data;
-        // console.log(this.props)
+        const {index} = this.state;
+        const dataWithColor = data.map((d, i) => ( {...d, color : Number(i !== index)}));
         const chartWidth = 800;
         const chartHeight = 500;
         const chartDomain = [0, chartHeight];
@@ -21,10 +28,13 @@ class BarGraph extends React.Component {
                 width={chartWidth}
                 height={chartHeight}
                 yDomain={chartDomain}
+                onMouseLeave = {() => this.setState({index : null})}
             >
                 <XAxis />
                 <YAxis />
-                <VerticalBarSeries data={data}/>
+                <VerticalBarSeries data={dataWithColor}
+                onNearestX={(d, {index}) => this.setState({index})}
+                />
                 <LabelSeries
           data={data.map((obj) => {
             return { ...obj, label: obj.y.toString() };
