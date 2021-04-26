b4df542200a8951522278d886cb0ab0e1b037752
HeatherMikan <heather.mikan@gmail.com>
2021-04-25 21:05:34 -0400

    bar graph pop up hover

--- frontend/src/components/BarGraph.js
+++ frontend/src/components/BarGraph.js
@@ -1,61 +1,64 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import 'react-vis/dist/style.css';
+import { dsv } from 'd3-fetch';
 import {
-    XYPlot,
-    XAxis,
-    YAxis,
-    VerticalBarSeries,
-    LabelSeries
-} from "react-vis";
+  XYPlot,
+  VerticalBarSeries,
+  LineMarkSeries,
+  XAxis,
+  YAxis,
+  Hint,
+} from 'react-vis';
 
-class BarGraph extends React.Component {
-  constructor() {
-    super();
+class BarGraph extends Component {
+  constructor(props) {
+    super(props);
+
     this.state = {
-      index: null,
-    }
-  }
-    render () {
-        const data = this.props.data;
-        // console.log(data)
-        // console.log(data[0])
-        const {index} = this.state;
-        var maxHeight = 0
-        const dataWithColor = data.map(function(d, i) { 
-            if (d.y > maxHeight) {
-              maxHeight = d.y
-            }
-            return ({x: d.x, y: d.y, color : Number(i !== index)})
-          });
-        // console.log(dataWithColor)
-        const chartWidth = 800;
-        const chartHeight = 500;
-        const chartDomain = [0, maxHeight];
-        return (
-            <XYPlot
-                xType="ordinal"
-                width={chartWidth}
-                height={chartHeight}
-                yDomain={chartDomain}
-                onMouseLeave = {() => this.setState({index : null})}
-            >
-                <XAxis />
-                <YAxis />
-                <VerticalBarSeries data={dataWithColor}
-                 onNearestX={(d, {index}) => this.setState({index})}
-                />
-                <LabelSeries
-          data={dataWithColor.map((obj) => {
-            // console.log(obj)
-            return { ...obj, label: obj.y.toString() };
-          })}
-          labelAnchorX="middle"
-          labelAnchorY="text-after-edge"
-        />
-            </XYPlot>
-        )
+      data: props.data,
+      hoverData: null,
+    };
 
+ 
+  }
+  render() {
+    const {
+      data,
+      hoverData
+    } = this.state;
+
+    if(!data) {
+      return (<div className="App">
+        Loading...
+      </div>)
     }
+
+    return (
+      <div className="App">
+        <XYPlot
+          width={1200}
+          height={600}
+          margin={{
+            left: 70
+          }}
+          xType="ordinal"
+          onMouseLeave={() => this.setState({hoverData: null})}
+        >
+          {
+            <VerticalBarSeries
+              onValueMouseOver={(d) => this.setState({hoverData: d})}
+              data={data}
+            />
+          }
+
+          {!!hoverData && <Hint value={hoverData} />}
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      </div>
+    );
+
+  }
 }
 
 export default BarGraph;
